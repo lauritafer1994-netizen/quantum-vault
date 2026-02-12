94ae516d06abc3e42d9effd15c2e619a92627e44
lauritafer1994-netizen <lauritafer1994@gmail.com>
2026-02-11 20:47:36 -0800

    Create src/utils/metaplex.js with Metaplex integration functions

---
+++ src/utils/metaplex.js
@@ -0,0 +1,15 @@
+import { SystemProgram, SYSVAR_RENT_PUBKEY } from '@solana/web3.js';
+
+// Metadata functions for Metaplex integration
+
+export const createMetadata = async (data) => {
+    // Implementation for creating metadata
+};
+
+export const updateMetadata = async (data) => {
+    // Implementation for updating metadata
+};
+
+export const getMetadata = async (metadataAddress) => {
+    // Implementation for retrieving metadata
+};
